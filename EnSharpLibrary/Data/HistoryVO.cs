a52bb0980cbe0c82813f9c2e2db9c037e73b7fc0
deanKWON <33994508+deanKWON@users.noreply.github.com>
2018-05-03 12:18:27 +0900

    HistoryVO 생성

---
+++ EnSharpLibrary/Data/HistoryVO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnSharpLibrary.Data
+{
+    class HistoryVO
+    {
+        private int memberID;
+        private float bookID;
+        private DateTime dateBorrowed;
+        private DateTime dateReturn;
+        private DateTime dateDeadlineForReturn;
+        private int numberOfRenew;
+
+        public int MemberID
+        {
+            get { return memberID; }
+        }
+
+        public float BookID
+        {
+            get { return bookID; }
+        }
+
+        public DateTime DateBorrowed
+        {
+            get { return dateBorrowed; }
+        }
+
+        public DateTime DateReturn
+        {
+            get { return dateReturn; }
+            set { dateReturn = value; }
+        }
+
+        public DateTime DateDeadlineForReturn
+        {
+            get { return dateDeadlineForReturn; }
+            set { dateDeadlineForReturn = value; }
+        }
+            
+    }
+}
